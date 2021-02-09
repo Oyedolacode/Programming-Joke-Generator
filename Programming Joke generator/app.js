0de31cbaf2e34aa26903db51d5b0ca02b731dc04
Oyedolacode <segunzyshegszy12@gmail.com>
2021-02-08 18:53:57 -0800

    fixed Api issues

---
+++ Programming Joke generator/app.js
@@ -0,0 +1,24 @@
+const jokeText = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+jokeBtn.addEventListener("click", generateJokes)
+
+generateJokes();
+
+async function generateJokes() {
+    const res = await fetch(
+        "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
+    )
+    const data = await res.json();
+    console.log(data)
+    console.log(data.joke)
+    console.log(data.delivery)
+    let joke = " "
+    if (data.joke == undefined) {
+        joke = `${data.setup} br /> ${data.delivery}`
+    } else {
+        joke = data.joke
+    }
+    jokeText.innerHTML = joke
+
+    }
